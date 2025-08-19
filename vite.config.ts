cdc32555fca0b93d5c2e88022916517a5604e913
liviucozac <liviucozac28@gmail.com>
2025-08-19 22:38:25 +0300

    Update scanner to accept all comenzi.farmaciatei.ro QR codes

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/myTei/',   // 👈 this makes it work on GitHub Pages
   plugins: [react()],
 })
